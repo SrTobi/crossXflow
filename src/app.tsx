18321f2453ef1b6512d94a73bbf28121cd18ee97
Richard Gebauer <richardgebauer@gmx.de>
2018-03-25 12:58:51 +0200

    Implemented fullscreen, fixed loading

--- src/app.tsx
+++ src/app.tsx
@@ -14,6 +14,6 @@ export class App extends React.Component<{}, AppState> {
   };
 
   render() {
-    return <div className="container">{this.state.gameView}</div>;
+    return this.state.gameView;
   }
 }
--- src/gameview.tsx
+++ src/gameview.tsx
@@ -1,97 +1,108 @@
 import * as React from "react";
-import * as PIXI from 'pixi.js';
-import * as Utils from './utils';
-
+import * as PIXI from "pixi.js";
+import * as Utils from "./utils";
 
 export abstract class State {
-    enter(prev: State, renderer: PIXI.SystemRenderer): void {}
-    leave(next: State): void {}
+  enter(prev: State, renderer: PIXI.SystemRenderer): void {}
+  leave(next: State): void {}
 
-    resized(renderer: PIXI.SystemRenderer): void {}
+  resized(renderer: PIXI.SystemRenderer): void {}
 
-    abstract update(dt: number): State | null;
-    abstract render(dt: number): PIXI.Container | null;
+  abstract update(dt: number): State | null;
+  abstract render(dt: number): PIXI.Container | null;
 }
 
-export class GameView extends React.Component<{firstState: State}, {}>{
+export class GameView extends React.Component<{ firstState: State }, {}> {
+  private renderer: PIXI.SystemRenderer;
+  private lastUpdate: number;
+  private currentState: State;
+  private gameCanvas: HTMLElement | null;
 
-    private renderer: PIXI.SystemRenderer;
-    private lastUpdate: number;
-    private currentState: State;
-    private gameCanvas: HTMLElement | null;
+  constructor(props: { firstState: State }) {
+    super(props);
+  }
 
-    constructor(props: {firstState: State}) {
-        super(props);
+  private run(state: State) {
+    this.setGameState(state);
+    this.lastUpdate = Date.now();
+    this.onFrame();
+  }
+
+  private setGameState(nextState: State) {
+    let prevState = this.currentState;
+    if (prevState) {
+      prevState.leave(nextState);
     }
-
-    private run(state: State) {
-        this.setGameState(state);
-        this.lastUpdate = Date.now();
-        this.onFrame();
+    this.currentState = nextState;
+    if (nextState) {
+      nextState.enter(prevState, this.renderer);
+      nextState.resized(this.renderer);
     }
+  }
 
-    private setGameState(nextState: State) {
-        let prevState = this.currentState;
-        if(prevState) {
-            prevState.leave(nextState);
-        }
-        this.currentState = nextState;
-        if(nextState) {
-            nextState.enter(prevState, this.renderer);
-            nextState.resized(this.renderer);
-        }
+  private resized() {
+    if (!this.gameCanvas) {
+      return;
     }
-
-    private resized() {
-        if(!this.gameCanvas) {
-            return;
-        }
-        this.renderer.resize(this.gameCanvas.clientWidth, this.gameCanvas.clientWidth * 0.7);
-        if(this.currentState) {
-            this.currentState.resized(this.renderer);
-        }
+    this.renderer.resize(
+      window.innerWidth, // this.gameCanvas.clientWidth,
+      window.innerHeight // this.gameCanvas.clientHeight //this.gameCanvas.clientWidth * 0.7
+    );
+    if (this.currentState) {
+      this.currentState.resized(this.renderer);
     }
+  }
 
-    private onFrame() {
-        let now = Date.now();
-        let dt = (now - this.lastUpdate) / 1000;
-        this.lastUpdate = now;
+  private onFrame() {
+    let now = Date.now();
+    let dt = (now - this.lastUpdate) / 1000;
+    this.lastUpdate = now;
 
-        if(this.currentState) {
-            // render
-            let stage = this.currentState.render(dt);
-            if(stage) {
-                this.renderer.render(stage);
-            }
+    if (this.currentState) {
+      // render
+      let stage = this.currentState.render(dt);
+      if (stage) {
+        this.renderer.render(stage);
+      }
 
-            // update
-            let nextState = this.currentState.update(dt);
-            if(nextState) {
-                this.setGameState(nextState);
-            }
+      // update
+      let nextState = this.currentState.update(dt);
+      if (nextState) {
+        this.setGameState(nextState);
+      }
 
-            // reqest next frame
-            requestAnimationFrame(this.onFrame.bind(this));
-        }
+      // reqest next frame
+      requestAnimationFrame(this.onFrame.bind(this));
     }
+  }
 
-    componentDidMount() {
-        this.renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x000000, antialias: true});
+  componentDidMount() {
+    this.renderer = PIXI.autoDetectRenderer(
+      window.innerWidth,
+      window.innerHeight,
+      {
+        backgroundColor: 0x000000,
+        antialias: true
+      }
+    );
 
-        this.gameCanvas!.appendChild(this.renderer.view);
-        this.resized();
-        //this.renderer.resize(this.gameCanvas.clientWidth, this.gameCanvas.clientHeight);
-        window.onresize = () => {this.resized();}
+    this.gameCanvas!.appendChild(this.renderer.view);
+    this.resized();
+    //this.renderer.resize(this.gameCanvas.clientWidth, this.gameCanvas.clientHeight);
+    window.onresize = () => {
+      this.resized();
+    };
 
-        console.log("componentDidMount");
-        
-        this.run(this.props.firstState);
-    }
+    console.log("componentDidMount");
 
-    render() {
-        return (
-                <div className="game-canvas-container" ref={(c) => this.gameCanvas = c}>
-                </div>
-            );
-    }
+    this.run(this.props.firstState);
+
+    setTimeout(() => this.resized(), 200);
+  }
+
+  render() {
+    return (
+      <div className="game-canvas-container" ref={c => (this.gameCanvas = c)} />
+    );
+  }
 }
